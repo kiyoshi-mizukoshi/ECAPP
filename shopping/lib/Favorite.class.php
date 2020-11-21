2afe39ee3c21e8261db0112e5fdb270e08c9669d
kiyoshi-mizukoshi <928kiyoshi@gmail.com>
2020-11-21 14:36:58 +0900

    ほしい物リストを作る準備

---
+++ shopping/lib/Favorite.class.php
@@ -0,0 +1,28 @@
+<?php 
+namespace shopping\lib;
+
+class Favorite
+{
+  private $db = null;
+
+  public function __construct($db = null)
+  {
+    $this->db = $db;
+  }
+
+  public function insertFavorite()
+  {
+    
+  }
+
+  public function selectFavorite()
+  {
+
+  }
+
+  
+}
+
+
+
+?>
