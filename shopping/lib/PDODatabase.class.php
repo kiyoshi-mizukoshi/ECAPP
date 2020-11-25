a679066268a2dc157f457c0cc4bd233c89487ea1
kiyoshi-mizukoshi <928kiyoshi@gmail.com>
2020-11-25 18:36:45 +0900

    heroku関連の編集

--- shopping/lib/PDODatabase.class.php
+++ shopping/lib/PDODatabase.class.php
@@ -5,47 +5,69 @@ namespace shopping\lib;
 class PDODatabase
 {
   
-  private $dbh = null;
-  private $order = '';
-  private $limit = '';
-  private $offset = '';
-  private $groupby = '';
+ private $dbh = null;
+    private $order = "";
+    private $limit = "";
+    private $offset = "";
+    private $groupby = "";
 
-  public function __construct($db_host,$db_user,$db_pass,$db_name,$db_type)
-  {
-    $this->dbh = $this->connectDB($db_host,$db_user,$db_pass,$db_name,$db_type);
-    $this->db_host = $db_host;
-    $this->db_user = $db_user;
-    $this->db_pass = $db_pass;
-    $this->db_name = $db_name;
-    //SQL関連
-    $this->order = '';
-    $this->limit = '';
-    $this->offset = '';
-    $this->groupby = '';
-  }
-
-  private function connectDB($db_host,$db_user,$db_pass,$db_name,$db_type)
-  {
-    try {  //接続エラー発生→PDOExceptionオブジェクトがスローされる→例外処理をキャッチする
-
-      switch ($db_type) {
-        case 'mysql':
-          $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name;
-          $dbh = new \PDO($dsn,$db_user,$db_pass);
-          $dbh->query('SET NAMES utf8');
-        break;
-
-        case 'pgsql':
-          $dsn = 'pgsql:dbname=' . $db_name . 'host=' . $db_host . 'port=5432';
-          $dbh = new \PDO($dsn,$db_user,$db_pass);
-        break;
-      }
-    }catch (\PDOException $e) {
-      var_dump($e->getMessage());
-      exit();
+    public function __construct(
+        $DB_HOST,
+        $DB_USERNAME,
+        $DB_PASSWORD,
+        $DB_DATABASE,
+        $db_type
+    ) {
+        $this->dbh = $this->connectDB(
+            $DB_HOST,
+            $DB_USERNAME,
+            $DB_PASSWORD,
+            $DB_DATABASE,
+            $db_type
+        );
+        $this->DB_HOST = $DB_HOST;
+        $this->DB_USERNAME = $DB_USERNAME;
+        $this->DB_PASSWORD = $DB_PASSWORD;
+        $this->DB_DATABASE = $DB_DATABASE;
+        //SQL関連
+        $this->order = "";
+        $this->limit = "";
+        $this->offset = "";
+        $this->groupby = "";
     }
-    return $dbh;
+
+    private function connectDB(
+        $DB_HOST,
+        $DB_USERNAME,
+        $DB_PASSWORD,
+        $DB_DATABASE,
+        $db_type
+    ) {
+        try {
+            //接続エラー発生→PDOExceptionオブジェクトがスローされる→例外処理をキャッチする
+
+            switch ($db_type) {
+                case "mysql":
+                    $dsn = "mysql:host=" . $DB_HOST . ";dbname=" . $DB_DATABASE;
+                    $dbh = new \PDO($dsn, $DB_USERNAME, $DB_PASSWORD);
+                    $dbh->query("SET NAMES utf8");
+                    break;
+
+                case "pgsql":
+                    $dsn =
+                        "pgsql:dbname=" .
+                        $DB_DATABASE .
+                        "host=" .
+                        $DB_HOST .
+                        "port=5432";
+                    $dbh = new \PDO($dsn, $DB_USERNAME, $DB_PASSWORD);
+                    break;
+            }
+        } catch (\PDOException $e) {
+            var_dump($e->getMessage());
+            exit();
+        }
+        return $dbh;
   }
 
   public function setQuery($query = '' , $arrVal = [])
